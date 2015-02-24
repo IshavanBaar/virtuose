6f0de2f3454ea790a973d9a451bf99ff4a9429ad
Isha van Baar <isha.vanbaar@gmail.com>
2015-02-24 15:54:54 +0100

    Test 10: 235

--- js/JSKeyDrawer.js
+++ js/JSKeyDrawer.js
@@ -1,4 +1,4 @@
-this.screenWidth = $(window).width() - 230; 
+this.screenWidth = $(window).width() - 235; 
 this.screenHeight = $(window).height(); 
 
 //Other variables with dummy values.
--- js/JSNoteMapper.js
+++ js/JSNoteMapper.js
@@ -11,7 +11,7 @@ this.currentMIDINumbers = [];
 this.currentWhiteMIDINumbers = [];
 this.numberOfWhiteKeys = -1;
 
-this.screenWidth = $(window).width() - 230; 
+this.screenWidth = $(window).width() - 235; 
 this.screenHeight = $(window).height(); 
 this.keyWidth = -1;
 
